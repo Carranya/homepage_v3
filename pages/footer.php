2d3a97a12a73d1aeda8a9b47a6925033fd35ae96
Carranya <k.gong82@gmail.com>
2023-01-31 20:02:12 +0100

    add copyright

--- pages/footer.php
+++ pages/footer.php
@@ -1,23 +1,26 @@
-<div id='idContact' class='flex justify-center bg-violet-500 text-sm'>
-    <div class='m-3 p-3'>
-        Karin Giang<br>
-        Schützenmattstrasse 7<br>
-        8302 Kloten<br>
-        Schweiz
-    </div>
+<div id='idContact' class='bg-violet-500 text-sm'>
+    <div class='flex justify-center'>
+        <div class='m-3 p-3'>
+            Karin Giang<br>
+            Schützenmattstrasse 7<br>
+            8302 Kloten<br>
+            Schweiz
+        </div>
 
-    <div class='m-3 p-3'>
-        <div class='flex items-center'>
-            <img src='img/icons/phone.png' class='w-5'>
-            : 078 / 823 67 56
-        </div>
-        <div class='flex items-center'>
-            <img src='img/icons/mail.png' class='w-5'>
-            <a href='mailto:karin.giang1982@gmail.com'>: karin.giang1982@gmail.com</a>
-        </div>
-        <div class='flex items-center mt-2'>
-            <a href='' class='mr-2'><img src='img/icons/github.png' class='w-8 hover:opacity-40'></a>
-            <a href='' class='mr-2'><img src='img/icons/linkedin.png' class='w-8 hover:opacity-40'></a>
+        <div class='m-3 p-3'>
+            <div class='flex items-center'>
+                <img src='img/icons/phone.png' class='w-5'>
+                : 078 / 823 67 56
+            </div>
+            <div class='flex items-center'>
+                <img src='img/icons/mail.png' class='w-5'>
+                <a href='mailto:karin.giang1982@gmail.com'>: karin.giang1982@gmail.com</a>
+            </div>
+            <div class='flex items-center mt-2'>
+                <a href='' class='mr-2'><img src='img/icons/github.png' class='w-8 hover:opacity-40'></a>
+                <a href='' class='mr-2'><img src='img/icons/linkedin.png' class='w-8 hover:opacity-40'></a>
+            </div>
         </div>
     </div>
+    <div class='p-3 text-center'>Karin Giang ©2023</div>
 </div>
