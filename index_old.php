6c2365f82af665aef6ebde105493b5e88a3ea53f
Carranya <k.gong82@gmail.com>
2023-08-06 15:12:41 +0200

    homepage updated

--- index_old.php
+++ index_old.php
@@ -9,17 +9,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Karin Giang</title>
+    <title>Karin Giang - Webentwicklerin</title>
     <link rel="icon" type="image/x-icon" href="img/icons/favicon.png">
     <link rel="stylesheet" href="css/output.css">
     <script type='text/javascript' src='js/navi.js'></script>
 </head>
 <body>
     <?php
-        // include 'pages/nav.php';
-        // include 'pages/home.php';
-        // include 'pages/portfolio.php';
-        // include 'pages/it.php';
+        include 'pages/nav.php';
+        include 'pages/home.php';
+        include 'pages/portfolio.php';
+        include 'pages/it.php';
         include 'pages/about.php';
         include 'pages/footer.php';
     ?>
---
+++ pages/about_old.php
@@ -0,0 +1,25 @@
+<div id='idAbout' class='bg-gradient-to-r from-red-300 to-violet-300 lg:h-screen pb-16'>
+    <div class='flex justify-center'>
+        <div class='w-1/2'>
+            <h1 class='mt-16 mb-5 text-4xl font-bold underline text-center italic text-blue-800'>Über mich</h1>
+            <span class='flex justify-center md:flex lg:block lg:float-right'><img src='img/karin.jpg' title='Karin Giang' class='w-80 rounded-xl ml-5 mb-5'></span>
+            <p class='text-blue-800'>
+                Guten Tag, ich bin Karin Giang.
+                <br><br>
+                Ich bin eine Programmiererin und Webentwicklerin.
+                <br><br>
+                Momentan bin ich auf der Suche nach einer Firma, die bereit ist, 
+                mir den Einstieg in die Informatik Branche zu ermöglichen.
+                Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik absolviert, 
+                aber ich konnte privat durch Selbststudium das Programmieren von HTML, CSS, PHP, MySQL, Javascript etc. aneignen.
+                <br><br>
+                In meiner Freizeit programmiere ich sehr gerne und lerne immer neue Techniken und Programmiersprachen kennen.
+                Bisher habe ich bereits kleine Webtools sowie auch Webpages programmiert.    
+                <br><br>
+                In offiziellen Dokumenten verwende ich noch den männlichen Namen Martin.
+                Jedoch in der Freizeit werden meine Freunde mich immer mehr als weiblich ansprechen und ansehen.
+                Momentan bin ich noch sehr beschäftigt mit der beruflichen Wandel. Sobald ich mit der neuen Beruf stabilisiert habe, werde ich mit der Hormontheraphie beginnen.
+            </p>
+        </div>
+    </div>
+</div>
